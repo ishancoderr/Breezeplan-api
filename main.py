95222e8c92b4e7dde5662e1d797e3e6afbc6e020
Ishan Thathsara Weerakoon <31079429+ishancoderr@users.noreply.github.com>
2025-01-23 21:01:31 +0100

    add CORS to support ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.weather import router as weather_router
 from app.routers.activitySuggestions import router as activity_suggestions_router
 
@@ -6,6 +7,15 @@ app = FastAPI(
     title="Breezeplan API",
     version="1.0.0.v",
     description="API for weather data and activity suggestions."
+)
+
+# Enable CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, change to specific frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(weather_router)
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@ activate virtual environment:
 .\env\Scripts\activate
 
 update requirements txt file
-pip freeze > requirements.txt
+pip freeze > requirement.txt
 
 docker-compose down  # Stop containers
 docker-compose build --no-cache  # Rebuild without cache
